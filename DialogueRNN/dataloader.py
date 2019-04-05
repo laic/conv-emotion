Adapations for CallHome. fixed bugs relating to masking

DialogueRNN/dataloader.py
@@ -3,6 +3,134 @@ from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 import pickle
 import pandas as pd
+import numpy as np
+import h5py 
+
+class CallHomeDataset(Dataset):
+
+    def __init__(self, path, stance, part="dev", acproject=False, acfset="eGeMAPSv01a"):
+     
+        print("PART", part, acproject)   
+        filetemp = "mlp_[100]_STANCE_FEATNAME_eGeMAPSv01a_topseg_grustate_grustate_False_False_False_True_spksegid-segid_0.enc.h5"
+        filetemp = filetemp.replace("STANCE", stance) 
+
+
+        acfile = path + "/" + filetemp.replace("FEATNAME", "turnacoustic")
+        if acproject:
+            print("*** acproject")
+            acfile = acfile.replace("False_False_False_True", "False_False_True_True")
+
+        if acfset != "eGeMAPSv01a":
+            print("*** acfset", acfset)
+            acfile = acfile.replace("eGeMAPSv01a", acfset)
+
+
+        print(acfile)
+        hfac = h5py.File(acfile, "r")
+        partacdf, acnames = self.get_h5_part(hfac, part, featname="turnacoustic")       
+        hfac.close()
+ 
+        print(partacdf.head())
+        print(partacdf.shape)
+
+        txtfile = path + "/" + filetemp.replace("FEATNAME", "turntrans")
+        print(txtfile)
+        hftxt = h5py.File(txtfile, "r")
+        parttxtdf, txtnames = self.get_h5_part(hftxt, part, featname="turntrans")       
+        hftxt.close()
+
+        print(parttxtdf.head())
+        print(parttxtdf.shape)
+
+        partdf = parttxtdf.merge(partacdf, how='inner')
+        partdf['target'] = partdf['target'].astype(int)
+        partdf.loc[:,'xid'] = range(partdf.shape[0]) 
+
+        metacols = ["xid", "spksegid", "segid", "conv", "fname", "spk", "topic", "target"]
+        hfmeta = partdf[metacols]       
+        hfmeta.to_csv("./meta/" + stance + "." + part + ".meta.txt")
+ 
+        ## One dictionary per clip = fname 
+        self.part_clips = partdf.fname.unique()
+        self.part_acoustic = {} 
+        self.part_text = {} 
+        self.part_speakers = {}
+        self.part_labels = {}
+        self.part_meta = {}
+
+        #self.keys = self.part_clips[:100]
+        self.keys = self.part_clips 
+
+        for clip in self.keys:
+            #print(clip) 
+            currac = partdf[partdf.fname == clip][acnames]
+            self.part_acoustic[clip] = list(np.array(currac))
+
+            currtxt = partdf[partdf.fname == clip][txtnames]
+            self.part_text[clip] = list(np.array(currtxt))
+
+            currspk = partdf[partdf.fname == clip]['spk']
+            self.part_speakers[clip] = list(currspk)
+
+            currlabels = partdf[partdf.fname == clip]['target']
+            self.part_labels[clip] = list(currlabels)
+
+            currmeta = partdf[partdf.fname == clip][['xid']].astype(int)
+            self.part_meta[clip] = list(np.array(currmeta))
+
+        print(len(self.part_acoustic))
+        print(len(self.part_text))
+        print(len(self.part_speakers))
+        print(len(self.part_labels))
+        print(len(self.part_clips))
+        print(len(self.part_meta))
+
+        self.len = len(self.keys)
+
+        print("FINISHED INIT", part)
+
+    def __getitem__(self, index):
+        vid = self.keys[index]
+        return torch.FloatTensor(self.part_text[vid]),\
+               torch.FloatTensor(self.part_acoustic[vid]),\
+               torch.FloatTensor([[1,0] if x=='A' else [0,1] for x in\
+                                  self.part_speakers[vid]]),\
+               torch.FloatTensor([1]*len(self.part_labels[vid])),\
+               torch.LongTensor(self.part_labels[vid]) , \
+               torch.LongTensor(self.part_meta[vid])
+
+    def get_h5_part(self, hfac, part, featname='turnacoustic'):
+
+        part_acoustic = pd.DataFrame(np.array(hfac[part][featname]))
+        fnames = [ featname + str(i) for i in range(1,part_acoustic.shape[1]+1)]
+        part_acoustic = part_acoustic.rename(columns=dict(zip(part_acoustic.columns.values, fnames)))
+
+        metacols = ['spksegid','spksegid.1','segid','segid.1','conv','fname','spk','topic','stancename','target']
+        part_meta = pd.DataFrame(np.array(hfac[part]['meta']).astype(str), columns=metacols)
+        part_meta = part_meta.drop(['spksegid.1', 'segid.1'], axis=1)
+
+        partdf = pd.concat([part_meta, part_acoustic], axis=1)
+
+        starttimes = partdf.segid.apply(lambda u: int(u.split("_")[2].split("-")[0]))
+        partdf.loc[:,'starttime'] = starttimes
+
+        ## now we need to reorder based on starttimes
+        partdf = partdf.sort_values(by=['conv','fname','starttime'])
+        print(type(partdf))
+        print(type(fnames))
+        return (partdf, fnames)
+
+
+    def __len__(self):
+        return self.len
+
+    def collate_fn(self, data):
+        #print("collate_fn")
+        dat = pd.DataFrame(data)
+        #print(dat.head())
+        #print(dat.shape)
+        return [pad_sequence(dat[i]) if i<4 else pad_sequence(dat[i], True) for i in dat]
+
 
 class IEMOCAPDataset(Dataset):
 
@@ -12,6 +140,22 @@ class IEMOCAPDataset(Dataset):
         self.testVid = pickle.load(open(path, 'rb'), encoding='latin1')
         '''
         label index mapping = {'hap':0, 'sad':1, 'neu':2, 'ang':3, 'exc':4, 'fru':5}
+        '''
+        '''
+        curr = list(self.videoSpeakers)[0]
+        print(curr)
+        print(self.videoSpeakers[curr])
+
+        print(self.videoSpeakers[curr])
+        print(type(self.videoSpeakers[curr]))
+        print(self.videoLabels[curr])
+        print(type(self.videoLabels[curr]))
+        print(type(self.videoAudio[curr]))
+        print(type(self.videoAudio[curr][0]))
+        print(type(self.videoText[curr]))
+        print(type(self.videoText[curr][0]))
+
+        raise SystemExit
         '''
         self.keys = [x for x in (self.trainVid if train else self.testVid)]
 
@@ -41,6 +185,28 @@ class AVECDataset(Dataset):
         self.videoIDs, self.videoSpeakers, self.videoLabels, self.videoText,\
             self.videoAudio, self.videoVisual, self.videoSentence,\
             self.trainVid, self.testVid = pickle.load(open(path, 'rb'),encoding='latin1')
+
+
+        #print(self.videoAudio[2][0])
+        print(self.videoAudio[2][0].shape)
+        print(len(self.videoAudio))
+        #for k, v in self.videoAudio.items():
+        #        print(k, len(v))
+
+
+        print (len(self.trainVid))
+        print (len(self.testVid))
+
+        print(type(self.videoSpeakers))
+
+        print(self.videoSpeakers[2])
+        print(type(self.videoSpeakers[2]))
+        print(self.videoLabels[2])
+        print(type(self.videoLabels[2]))
+        print(type(self.videoAudio[2]))
+        print(type(self.videoAudio[2][0]))
+        print(type(self.videoText[2]))
+        print(type(self.videoText[2][0]))
 
         self.keys = [x for x in (self.trainVid if train else self.testVid)]
         self.len = len(self.keys)
@@ -112,7 +278,7 @@ class DailyDialogueDataset(Dataset):
     def __getitem__(self, index):
         conv = self.keys[index]
         
-        return torch.LongTensor(self.InputSequence[conv]), \
+        return  torch.LongTensor(self.InputSequence[conv]), \
                 torch.FloatTensor([[1,0] if x=='0' else [0,1] for x in self.Speakers[conv]]),\
                 torch.FloatTensor([1]*len(self.ActLabels[conv])), \
                 torch.LongTensor(self.ActLabels[conv]), \

DialogueRNN/model.py
@@ -45,6 +45,9 @@ class MatchingAttention(nn.Module):
         x -> (batch, cand_dim)
         mask -> (batch, seq_len)
         """
+
+        mask_ = mask.transpose(0,1) 
+
         if type(mask)==type(None):
             mask = torch.ones(M.size(1), M.size(0)).type(M.type())
 
@@ -53,16 +56,22 @@ class MatchingAttention(nn.Module):
             M_ = M.permute(1,2,0) # batch, vector, seqlen
             x_ = x.unsqueeze(1) # batch, 1, vector
             alpha = F.softmax(torch.bmm(x_, M_), dim=2) # batch, 1, seqlen
+
         elif self.att_type=='general':
             M_ = M.permute(1,2,0) # batch, mem_dim, seqlen
             x_ = self.transform(x).unsqueeze(1) # batch, 1, mem_dim
             alpha = F.softmax(torch.bmm(x_, M_), dim=2) # batch, 1, seqlen
+
         elif self.att_type=='general2':
+
             M_ = M.permute(1,2,0) # batch, mem_dim, seqlen
             x_ = self.transform(x).unsqueeze(1) # batch, 1, mem_dim
-            alpha_ = F.softmax((torch.bmm(x_, M_))*mask.unsqueeze(1), dim=2) # batch, 1, seqlen
-            alpha_masked = alpha_*mask.unsqueeze(1) # batch, 1, seqlen
+            tbmm = torch.bmm(x_,M_)
+
+            alpha_ = F.softmax((torch.bmm(x_, M_))*mask_.unsqueeze(1), dim=2) # batch, 1, seqlen
+            alpha_masked = alpha_*mask_.unsqueeze(1) # batch, 1, seqlen
             alpha_sum = torch.sum(alpha_masked, dim=2, keepdim=True) # batch, 1, 1
+
             alpha = alpha_masked/alpha_sum # batch, 1, 1 ; normalized
             #import ipdb;ipdb.set_trace()
         else:
@@ -73,7 +82,6 @@ class MatchingAttention(nn.Module):
             alpha = F.softmax(self.vector_prod(mx_a),1).transpose(1,2) # batch, 1, seqlen
 
         attn_pool = torch.bmm(alpha, M.transpose(0,1))[:,0,:] # batch, mem_dim
-
         return attn_pool, alpha
 
 
@@ -128,7 +136,7 @@ class DialogueRNNCell(nn.Module):
                 torch.zeros(U.size()[0],self.D_g).type(U.type()) if g_hist.size()[0]==0 else
                 g_hist[-1])
 
-        print(q0.shape)
+        #print(q0.shape)
 
         ## Apply dropout
         g_ = self.dropout(g_)
@@ -216,7 +224,7 @@ class DialogueRNN(nn.Module):
 class BiModel(nn.Module):
 
     def __init__(self, D_m, D_g, D_p, D_e, D_h,
-                 n_classes=7, listener_state=False, context_attention='simple', D_a=100, dropout_rec=0.5,
+                 n_classes=2, listener_state=False, context_attention='simple', D_a=100, dropout_rec=0.5,
                  dropout=0.5):
         super(BiModel, self).__init__()
 
@@ -240,37 +248,49 @@ class BiModel(nn.Module):
         """
         X -> seq_len, batch, dim
         mask -> batch, seq_len
+        
         """
+        #print("REVERSE")
+        #print(mask)
+        #print(X.shape, mask.shape)
         X_ = X.transpose(0,1)
-        mask_sum = torch.sum(mask, 1).int()
+        #print(X_.shape)
+        mask_sum = torch.sum(mask, 0).int()
+        #print(mask_sum)
 
         xfs = []
         for x, c in zip(X_, mask_sum):
+        #    print(x.shape, c)
             xf = torch.flip(x[:c], [0])
             xfs.append(xf)
 
         return pad_sequence(xfs)
 
 
-    def forward(self, U, qmask, umask,att2=True):
+    def forward(self, U, qmask, umask, att2=True):
         """
         U -> seq_len, batch, D_m
         qmask -> seq_len, batch, party
         """
-
         emotions_f, alpha_f = self.dialog_rnn_f(U, qmask) # seq_len, batch, D_e
+
+
         emotions_f = self.dropout_rec(emotions_f)
         rev_U = self._reverse_seq(U, umask)
         rev_qmask = self._reverse_seq(qmask, umask)
+
         emotions_b, alpha_b = self.dialog_rnn_r(rev_U, rev_qmask)
         emotions_b = self._reverse_seq(emotions_b, umask)
         emotions_b = self.dropout_rec(emotions_b)
+
         emotions = torch.cat([emotions_f,emotions_b],dim=-1)
+
         if att2:
             att_emotions = []
             alpha = []
             for t in emotions:
                 att_em, alpha_ = self.matchatt(emotions,t,mask=umask)
+
                 att_emotions.append(att_em.unsqueeze(0))
                 alpha.append(alpha_[:,0,:])
             att_emotions = torch.cat(att_emotions,dim=0)
@@ -485,6 +505,44 @@ class Model(nn.Module):
         log_prob = F.log_softmax(self.smax_fc(hidden), 2) # seq_len, batch, n_classes
         return log_prob
 
+class CallHomeModel(nn.Module):
+
+    def __init__(self, D_m, D_g, D_p, D_e, D_h, attr, listener_state=False,
+            context_attention='simple', D_a=100, dropout_rec=0.5, dropout=0.5):
+        super(CallHomeModel, self).__init__()
+
+        self.D_m         = D_m
+        self.D_g         = D_g
+        self.D_p         = D_p
+        self.D_e         = D_e
+        self.D_h         = D_h
+        self.attr        = attr
+        self.dropout     = nn.Dropout(dropout)
+        self.dropout_rec = nn.Dropout(dropout)
+
+        self.dialog_rnn  = DialogueRNN(D_m, D_g, D_p, D_e,listener_state,
+                                    context_attention, D_a, dropout_rec)
+
+        self.linear      = nn.Linear(D_e, D_h)
+        self.smax_fc     = nn.Linear(D_h, 1)
+
+    def forward(self, U, qmask):
+        """
+        U -> seq_len, batch, D_m
+        qmask -> seq_len, batch, party
+        """
+
+        emotions,_ = self.dialog_rnn(U, qmask) # seq_len, batch, D_e
+        emotions = self.dropout_rec(emotions)
+        hidden = torch.tanh(self.linear(emotions))
+        hidden = self.dropout(hidden)
+        if self.attr!=4:
+            pred = (self.smax_fc(hidden).squeeze()) # seq_len, batch
+        else:
+            pred = (self.smax_fc(hidden).squeeze()) # seq_len, batch
+        return pred.transpose(0,1).contiguous().view(-1)
+
+
 class AVECModel(nn.Module):
 
     def __init__(self, D_m, D_g, D_p, D_e, D_h, attr, listener_state=False,
@@ -499,8 +557,10 @@ class AVECModel(nn.Module):
         self.attr        = attr
         self.dropout     = nn.Dropout(dropout)
         self.dropout_rec = nn.Dropout(dropout)
+
         self.dialog_rnn  = DialogueRNN(D_m, D_g, D_p, D_e,listener_state,
                                     context_attention, D_a, dropout_rec)
+
         self.linear      = nn.Linear(D_e, D_h)
         self.smax_fc     = nn.Linear(D_h, 1)
 
@@ -534,7 +594,14 @@ class MaskedNLLLoss(nn.Module):
         target -> batch*seq_len
         mask -> batch, seq_len
         """
+#        print("MaskedNLLLoss") 
+#        print(pred.shape, target.shape, mask.shape)
+
+        #print(mask) 
         mask_ = mask.view(-1,1) # batch*seq_len, 1
+        #print(mask_)
+#        print(mask_.shape)
+
         if type(self.weight)==type(None):
             loss = self.loss(pred*mask_, target)/torch.sum(mask)
         else: